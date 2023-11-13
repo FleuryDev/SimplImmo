5c23c0707fca695a8c0415c05479eea4f69591ed
FleuryDev <fleurykpamegan@gmail.com>
2023-11-13 11:07:56 +0100

    first commt

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// import "../../node_modules/aos/src/js/aos.js";
+import "../../dist/aos/aos.js";
+// import 'boxicons';
+// import '../../node_modules/swiper/swiper-bundle.min.js';
+import "../../dist/swiper/swiper-bundle.min.js";
+
+aos.init();
